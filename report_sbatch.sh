3ea36932bb9626880bfb8b4ecd859500d50c1ff5
wonhyung64 <wonhyung64@icloud.com>
2025-11-03 16:03:40 +0900

    feat - expt

---
+++ report_sbatch.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+read -r -d '' SLURM_SCRIPT<<'EOF'
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --partition=gpu6,gpu2,gpu3,gpu4,gpu5,gpu1
+##
+#SBATCH --job-name=expt
+#SBATCH -o logs/s_%j.out
+#SBATCH -e logs/s_%j.err
+##
+#SBATCH --gres=gpu:4
+
+hostname
+date
+# Function to check the number of running processes
+check_jobs() {
+    jobs -r | wc -l
+}
+MAX_JOBS=4 # Maximum number of parallel jobs
+experiments=(
+
+EOF
+
+read -r -d '' EXECUTER<<'EOF'
+)
+for index in ${!experiments[*]}; do
+
+    while [ "$(check_jobs)" -ge "$MAX_JOBS" ]; do
+        echo "Max nodes ($MAX_JOBS) running. Waiting..."
+        sleep 1m
+    done
+
+    GPU_ID=$(( COUNTER % 4 ))
+    export CUDA_VISIBLE_DEVICES=$GPU_ID
+
+    echo "Launching on GPU $GPU_ID: "
+    echo ${experiments[$index]} 
+    ${experiments[$index]} &
+
+    (( COUNTER++ ))
+    sleep 5
+
+done
+wait
+
+EOF
+
+
+
+ENV=/home1/wonhyung64/anaconda3/envs/openmmlab/bin/python3
+DATA_DIR=/data/sasrec/data
+
+
+experiments=(
+
+"sasrec/main.py --random-seed=1"
+"sasrec/main.py --random-seed=2"
+"sasrec/main.py --random-seed=3"
+"sasrec/main.py --random-seed=4"
+
+)
+
+
+echo "$SLURM_SCRIPT" > runner.sh
+COUNTER=0
+
+for index in ${!experiments[*]}; do
+
+    echo "\"$ENV ${experiments[$index]} --data-dir=$DATA_DIR\"" >> runner.sh
+    (( COUNTER++ ))
+
+    if [ "$COUNTER" -eq 4  ]; then
+        echo "$EXECUTER" >> runner.sh
+        chmod +x runner.sh
+
+        while true; do
+            JOB_COUNT=$(qstat -u wonhyung64 | awk 'NR>5 {count++} END {print count}')
+
+            if [ "$JOB_COUNT" -ge 20 ]; then
+                echo "Max jobs (20) running. Waiting..."
+                sleep 1m
+            else
+                echo "Job count is $JOB_COUNT, submitting new jobs..."
+                break
+            fi
+        done
+
+        sbatch runner.sh
+        rm runner.sh
+
+        echo "$SLURM_SCRIPT" >> runner.sh
+        COUNTER=0
+
+    fi
+                        
+    sleep 1
+
+done
+
+echo "$EXECUTER" >> runner.sh
+chmod +x runner.sh
+
+while true; do
+    JOB_COUNT=$(qstat -u wonhyung64 | awk 'NR>5 {count++} END {print count}')
+
+    if [ "$JOB_COUNT" -ge 20 ]; then
+        echo "Max jobs (20) running. Waiting..."
+        sleep 1m
+    else
+        echo "Job count is $JOB_COUNT, submitting new jobs..."
+        break
+    fi
+done
+
+sbatch runner.sh
+rm runner.sh
+wait
